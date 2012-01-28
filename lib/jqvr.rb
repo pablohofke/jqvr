148716b00a74845ecb9879309ad7956a9f7e55b5
Pablo Hofke <pablohofke@gmail.com>
2012-01-27 23:06:26 -0200

    incluida possibilidade de mapear custom validators em arquivo initialize

--- lib/jqvr.rb
+++ lib/jqvr.rb
@@ -1,2 +1,16 @@
 module Jqvr
+  
+  autoload :DefaultValidatorsMaps,  'jqvr/default_validators_maps'
+  
+  
+  # Mapeia os validators com as rules do jqv
+  # map_validator[:presence]="required"
+  mattr_accessor :map_validator
+  @@map_validator={}
+  
+  # Permite configurar o Jqvr no initialize
+  def self.setup
+    yield self
+  end
+  
 end
--- test/jqvr_test.rb
+++ test/jqvr_test.rb
@@ -4,4 +4,11 @@ class JqvrTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, Jqvr
   end
+  
+  test 'setup block yields self' do
+    Jqvr.setup do |config|
+      assert_equal Jqvr, config
+    end
+  end
+  
 end
