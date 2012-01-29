2284f75f4e712c85197dbc7774d78e6b473128e2
Pablo Hofke <pablohofke@gmail.com>
2012-01-29 20:14:58 -0200

    remove tos métodos pois separa em outros módulos

--- test/jqvr_test.rb
+++ test/jqvr_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 
 class JqvrTest < ActiveSupport::TestCase
   
-  include Jqvr
-  
   test "truth" do
     assert_kind_of Module, Jqvr
   end
@@ -14,12 +12,12 @@ class JqvrTest < ActiveSupport::TestCase
     end
   end
   
-  test "map_validator deve incluir o validator_map na array mapped_validators" do
-    map_validator :presence, "required:true", :on
-    mapped_validator=mapped_validators.first
-    assert_equal mapped_validator.kind, :presence
-    assert_equal mapped_validator.rule,"required:true"
-    assert_equal mapped_validator.option,:on
-  end
+  # test "map_validator deve incluir o validator_map na array mapped_validators" do
+  #   map_validator :presence, "required:true", :on
+  #   mapped_validator=mapped_validators.first
+  #   assert_equal mapped_validator.kind, :presence
+  #   assert_equal mapped_validator.rule,"required:true"
+  #   assert_equal mapped_validator.option,:on
+  # end
   
 end
