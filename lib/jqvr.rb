4109d3dbb4362bb87fafec01785b7becccaf0bab
Pablo Hofke <pablohofke@gmail.com>
2012-01-29 20:12:09 -0200

    remove tos métodos pois separa em outros módulos

--- lib/jqvr.rb
+++ lib/jqvr.rb
@@ -1,25 +1,9 @@
-require 'jqvr/validator_map'
-
 module Jqvr
   
   autoload :ValidatorMap, 'jqvr/validator_map'
-  autoload :Validators, 'jqvr/validators'
-  
-  # Mapeia os validators com as rules do jqv
-  # map_validator[:presence]="required:true"
-  mattr_reader :mapped_validators
-  # @@mapped_validators=[]
-  
-  def self.map_validator(kind,rule,option=nil)
-    @@mapped_validators=[] if @@mapped_validators.blank?
-    vm=ValidatorMap.new kind, rule, option
-    @@mapped_validators << vm
-  end
-  
-  def teste
-    "sucesso"
-  end
-  
+  autoload :BuiltInValidators, 'jqvr/built_in_validators'
+  autoload :MappedValidators, 'jqvr/mapped_validators'
+    
   # Permite configurar o Jqvr no initialize
   def self.setup
     yield self
