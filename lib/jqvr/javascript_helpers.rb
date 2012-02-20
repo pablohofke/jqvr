990a89095a9148515ffcbb9d85c7804c10345ddb
Pablo Hofke <pablohofke@gmail.com>
2012-02-20 09:31:31 -0200

    first commit

---
+++ lib/jqvr/javascript_helpers.rb
@@ -0,0 +1,9 @@
+module Jqvr
+  module JavascriptHelpers
+    # Adiciona a rule do validator
+    # add_rule :presence, :name, "the message"
+    def add_rule(kind,attribute,message)
+      
+    end
+  end
+end
---
+++ test/javascript_helpers_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class JavascriptHelpersTest < ActiveSupport::TestCase
+  
+  test "add_rule deve retornar a rule do valodator" do
+    
+    
+  end
+  
+end
