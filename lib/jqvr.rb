af3ebaaf76b7aaa5ce8f6f961d76daef311d0448
Pablo Hofke <pablohofke@gmail.com>
2012-02-20 09:28:57 -0200

    excluido map validator do jqvr

--- lib/jqvr.rb
+++ lib/jqvr.rb
@@ -1,8 +1,13 @@
+# require 'action_view'
+require 'jqvr/form_builder'
+
 module Jqvr
   
   autoload :ValidatorMap, 'jqvr/validator_map'
   autoload :BuiltInValidators, 'jqvr/built_in_validators'
   autoload :MappedValidators, 'jqvr/mapped_validators'
+  autoload :FormBuilder, 'jqvr/form_builder'
+  autoload :JavascriptHelpers, 'jqvr/javascript_helpers'
     
   # Permite configurar o Jqvr no initialize
   def self.setup
--- test/jqvr_test.rb
+++ test/jqvr_test.rb
@@ -12,12 +12,6 @@ class JqvrTest < ActiveSupport::TestCase
     end
   end
   
-  # test "map_validator deve incluir o validator_map na array mapped_validators" do
-  #   map_validator :presence, "required:true", :on
-  #   mapped_validator=mapped_validators.first
-  #   assert_equal mapped_validator.kind, :presence
-  #   assert_equal mapped_validator.rule,"required:true"
-  #   assert_equal mapped_validator.option,:on
-  # end
+
   
 end
