3746e56c84c8a7a7def4a03c916ea632557c37f2
Pablo Hofke <pablohofke@gmail.com>
2012-02-20 09:30:56 -0200

    adicionado FormBuilder

---
+++ lib/jqvr/form_builder.rb
@@ -0,0 +1,7 @@
+module Jqvr
+  class ActionView::Helpers::FormBuilder
+    def jqvr
+      # TODO Fazer primeiro as rules, sem colocar no javascript ainda para poder conferir o resultado
+    end
+  end
+end
