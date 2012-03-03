0f869ae2e020be4a573255e9dfe0ce4fcc824ac5
Pablo Hofke <pablohofke@gmail.com>
2012-03-03 09:53:39 -0300

    trocadas as funções new_method_rule_output por expected_rule ainda não testado

--- test/javascript_helpers_test.rb
+++ test/javascript_helpers_test.rb
@@ -37,15 +37,15 @@ class JavascriptHelpersTest < ActiveSupport::TestCase
     # exclusion
     assert_equal(expected_rule("exclusionIn","[\"Alto\", \"Médio\", \"Baixo\"]"),
                  rule_to_check(:exclusion, :in => ["Alto","Médio","Baixo"]))
-    # TODO Resolver este que não está passando
     # format with
     assert_equal(expected_rule("formatWith","/[A-Z]/"),
                  rule_to_check(:format,:with => /[A-Z]/))
+    # TODO Resolver este que não está passando
     # format without
-    assert_equal(new_method_rule_output("formatWithout","function(value){if(value!=""){return !value.match(/[A-Z]/)}else{return true}}"),
+    assert_equal(expected_rule("formatWithout","/[A-Z]/"),
                  rule_to_check(:format, :without => /[A-Z]/))
     # inclusion
-    assert_equal(new_method_rule_output("inclusionIn","function(value){if(value!=""){return jQuery.inArray(value,['Alto','Médio','Baixo'])!=-1}else{return true}}"),
+    assert_equal(expected_rule("inclusionIn","[\"Alto\", \"Médio\", \"Baixo\"]"),
                  rule_to_check(:inclusion, :in => ["Alto","Médio","Baixo"]))
     # length minimum
     assert_equal(expected_rule("minlength",3),
@@ -60,31 +60,31 @@ class JavascriptHelpersTest < ActiveSupport::TestCase
      assert_equal(expected_rule("rangelength","[3,5]"),
                   rule_to_check(:length,:in => [3,5]))                  
     # numericality
-    assert_equal(new_method_rule_output("numericality","function(value){if(value!=""){Number(value.replace(',', '.'))!=NaN}else{return true}}"),
+    assert_equal(expected_rule("numericality"),
                  rule_to_check(:numericality))
     # numericality only_integer
     assert_equal(expected_rule "digits",
                  rule_to_check(:numericality, :only_integer => true))
     # numericality greater_than
-    assert_equal(new_method_rule_output("numericalityGreaterThan","function(value,param){if(value!=''){return value > param}else{return true}}",5),
+    assert_equal(expected_rule("numericalityGreaterThan",5),
                  rule_to_check(:numericality, :greater_than => 5))
     # numericality greater_than_or_equal_to
     assert_equal(expected_rule("min",5),
                  rule_to_check(:numericality, :greater_than_or_equal_to => 5))    
     # numericality equal_to
-    assert_equal(new_method_rule_output("numericalityEqualTo","function(value,param){if(value!=''){return value==param}else{return true}}",5),
+    assert_equal(expected_rule("numericalityEqualTo",5),
                  rule_to_check(:numericality, :equal_to => 5))
     # numericality less_than
-    assert_equal(new_method_rule_output("numericalityLessThan","function(value,param){if(value!=''){return value < param}else{return true}}",5),
+    assert_equal(expected_rule("numericalityLessThan",5),
                  rule_to_check(:numericality, :less_than => 5))
     # numericality less_than_or_equal_to
     assert_equal(expected_rule("max",5),
                  rule_to_check(:numericality, :less_than_or_equal_to => 5))    
     # numericality odd
-    assert_equal(new_method_rule_output("numericalityOdd","function(value){if(value!=''){return value % 2!=0}else{return true}}",5),
+    assert_equal(expected_rule("numericalityOdd",5),
                  rule_to_check(:numericality, :odd => 5))
     # numericality even
-    assert_equal(new_method_rule_output("s","function(value){if(value!=''){return value % 2==0}else{return true}}",5),
+    assert_equal(expected_rule("numericalityEven",5),
                  rule_to_check(:numericality, :even => 5))
     # presence
     assert_equal(expected_rule "required", 
