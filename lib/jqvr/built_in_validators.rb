64998ad41bc9d24acf5df1ec7514a8dc42ae9fe9
Pablo Hofke <pablohofke@gmail.com>
2012-03-09 20:08:25 -0300

    agora os builtin validators que requiram functtions ficam separados em um arquivo coffeescript

--- lib/jqvr/built_in_validators.rb
+++ lib/jqvr/built_in_validators.rb
@@ -3,25 +3,25 @@ module Jqvr
   module BuiltInValidators
     
     # TODO Usando esses como base para criar add_rule e os testes
-    MappedValidators.add :acceptance, "required:true" #ok
-    MappedValidators.add :confirmation, "equalTo:%{attribute}_confirmation" #ok
-    MappedValidators.add :exclusion, "function(value){return jQuery.inArray(value,element,params)==-1}", :in #ok
-    MappedValidators.add :format, "function(value){if(value!=""){return value.match(%{options[:with]})}else{return true}}" #ok
-    MappedValidators.add :format, "function(value){if(value!=""){return !value.match(%{options[:without]})}else{return true}}" #ok
-    MappedValidators.add :inclusion, "function(value){if(value!=""){return jQuery.inArray(value,%{options[:in]})!=-1}else{return true}}" #ok
-    MappedValidators.add :length, "minlength:%{options[:minimum]}" #ok
-    MappedValidators.add :length, "maxlength:%{options[:maximum]}" #ok
-    MappedValidators.add :length, "rangelength:[%{options[:minimum]},%{options[:maximum]}]", :within #ok
-    MappedValidators.add :length, "rangelength:[%{options[:minimum]},%{options[:maximum]}]", :in #ok
-    MappedValidators.add :numericality, "function(value){if(value!=""){Number(value.replace(',', '.'))!=NaN}else{return true}}" #ok
-    MappedValidators.add :numericality, "digits:true",:only_integer #ok
-    MappedValidators.add :numericality, "function(value,param){if(value!=''){return value > param}else{return true}}", :greater_then #ok
-    MappedValidators.add :numericality, "min:%{options[:greater_than_or_equal_to]}" #ok
-    MappedValidators.add :numericality, "function(value,param){if(value!=''){return value==param}else{return true}}",:equal_to #ok
-    MappedValidators.add :numericality, "function(value,param){if(value!=''){return value < param}else{return true}}",:less_than #ok
-    MappedValidators.add :numericality, "max:%{options[:less_than_or_equal_to]}" #ok
-    MappedValidators.add :numericality, "function(value){if(value!=''){return value % 2!=0}else{return true}}",:odd
-    MappedValidators.add :numericality, "function(value){if(value!=''){return value % 2==0}else{return true}}",:even
+    MappedValidators.add :acceptance, "required:true" 
+    MappedValidators.add :confirmation, "equalTo:%{attribute}_confirmation" 
+    MappedValidators.add :exclusion, "exclusionIn:%{options[:in]}"
+    MappedValidators.add :format, "formatWith:%{options[with]}" 
+    MappedValidators.add :format, "formatWithout:%{options[without]}" 
+    MappedValidators.add :inclusion, "inclusion:%{options[:in]}" 
+    MappedValidators.add :length, "minlength:%{options[:minimum]}" 
+    MappedValidators.add :length, "maxlength:%{options[:maximum]}" 
+    MappedValidators.add :length, "rangelength:[%{options[:minimum]},%{options[:maximum]}]", :within 
+    MappedValidators.add :length, "rangelength:[%{options[:minimum]},%{options[:maximum]}]", :in 
+    MappedValidators.add :numericality, "numericality:true" 
+    MappedValidators.add :numericality, "digits:true",:only_integer 
+    MappedValidators.add :numericality, "numericalityGreterThen:%{options[:greater_then]}"
+    MappedValidators.add :numericality, "min:%{options[:greater_than_or_equal_to]}" 
+    MappedValidators.add :numericality, "numericalityEqualTo:%{options[:equal_to]}"
+    MappedValidators.add :numericality, "numericalityLessThen:%{options[:less_than]}"
+    MappedValidators.add :numericality, "max:%{options[:less_than_or_equal_to]}" 
+    MappedValidators.add :numericality, "numericalityOdd:true",:odd
+    MappedValidators.add :numericality, "numericalityEven:true",:even
     MappedValidators.add :presence,"required:true"
     # TODO Fazer depois, pois tem ajax
     # map_validator :uniqueness
