2361f1ac94d9e492b1b13b14d8544c101ee2bc00
Pablo Hofke <pablohofke@gmail.com>
2012-03-10 09:10:41 -0300

    inicio da construção do builder

--- lib/jqvr/form_builder.rb
+++ lib/jqvr/form_builder.rb
@@ -1,7 +1,16 @@
+require 'jqvr/javascript_helpers'
+
 module Jqvr
   class ActionView::Helpers::FormBuilder
     def jqvr
+      include Jqvr::Javascript::Helpers
       # TODO Fazer primeiro as rules, sem colocar no javascript ainda para poder conferir o resultado
+      @object.class.validators.each do |v|
+        v.map(&:attributes).first.each do |a|
+          # TODO terminar mensagem
+          add_rule v.map(&:kind).first, a, "the message"
+        end
+      end
     end
   end
 end
