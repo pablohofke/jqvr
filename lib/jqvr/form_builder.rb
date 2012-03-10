35c10b6071f951891862f056a773f05f628459d6
Pablo Hofke <pablohofke@gmail.com>
2012-03-10 18:41:43 -0300

    form_bulder coemeçando a mostrar as rules adicionadas

--- lib/jqvr/form_builder.rb
+++ lib/jqvr/form_builder.rb
@@ -1,16 +1,25 @@
 require 'jqvr/javascript_helpers'
 
 module Jqvr
+  
+  
+  
   class ActionView::Helpers::FormBuilder
+   include Jqvr::JavascriptHelpers
+    
     def jqvr
-      # include Jqvr::JavascriptHelpers
+      output=""
       # TODO Fazer primeiro as rules, sem colocar no javascript ainda para poder conferir o resultado
       @object.class.validators.each do |v|
         v.attributes.each do |a|
+          # debugger
           # TODO terminar mensagem
-          add_rule v.kind, a, "the message"
+          @method_name=a.to_s
+          output << add_rule(v.kind, a, "the message")
         end
       end
+      output
     end
+  
   end
 end
