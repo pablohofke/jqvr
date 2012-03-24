f98f02f5a4f50d8e4662609d911334a2fe642954
Pablo Hofke <pablohofke@gmail.com>
2012-03-24 07:50:47 -0300

    agora options_keys sempre vai retornar uma array

--- test/built_in_validators_test.rb
+++ test/built_in_validators_test.rb
@@ -9,10 +9,10 @@ class BuiltInValidatorsTest < ActiveSupport::TestCase
     BuiltInValidators
     assert_equal :acceptance, MappedValidators.all[0].kind
     assert_equal "required:true", MappedValidators.all[0].rule
-    assert_nil MappedValidators.all[0].options_keys
+    assert_equal [],MappedValidators.all[0].options_keys
     assert_equal :length, MappedValidators.all[7].kind
     assert_equal "maxlength:%{options[:maximum]}", MappedValidators.all[7].rule
-    assert_equal :maximum, MappedValidators.all[7].options_keys
+    assert_equal [:maximum], MappedValidators.all[7].options_keys
     assert_equal :length, MappedValidators.all[8].kind
     assert_equal "rangelength:[%{options[:minimum]},%{options[:maximum]}]", MappedValidators.all[8].rule
     assert_equal [:minimum,:maximum], MappedValidators.all[8].options_keys
