ab070868c09629fdfeb81238fa392a7c8c0f7597
Pablo Hofke <pablohofke@gmail.com>
2012-03-24 18:31:43 -0300

    iniciando os testes com uniquenesse, primeiro tentando viabilizar a chamada com ajax

--- lib/jqvr/form_builder.rb
+++ lib/jqvr/form_builder.rb
@@ -21,6 +21,19 @@ module Jqvr
       # TODO o debug:true é temporário
       "<script>
           jQuery(function(){
+              
+              jQuery.validator.addMethod('unequeness',function(value,element){
+                if (value!='')
+                  jQuery.ajax({
+                    url: 'http://0.0.0.0:3000/jqvr/validates/unequeness',
+                    dataType: 'json',
+                    data: {object: #{@object_name},element_name:element,element_value:value},
+                  });
+                else
+                  true
+                end
+              });
+              
               jQuery('##{ActionController::RecordIdentifier.dom_id(@object, @object.new_record? ? :new : :edit)}').validate({
                 debug: true,
                 //onfocusout: false,
